df2ecc900a315bdc7769d964a774c1099c9a17c1
Vinusha Indiran <vinusha88@gmail.com>
2024-09-03 21:11:30 -0400

    Add files via upload

---
+++ Generics/Generics/Group.cs
@@ -0,0 +1,28 @@
+﻿namespace Generics;
+
+public class Group<T> where T : struct
+{
+    private List<T> _items = new List<T>();
+
+    public void AddAnItem(T item)
+    {
+        _items.Add(item); 
+    }
+
+    public T GetAnItem(int index) 
+    {
+        if (index >= 0 && index < _items.Count)
+        {
+            return _items[index];
+        }
+        else
+        {
+            throw new IndexOutOfRangeException();
+        }
+    }
+
+    public List<T> GetSortedListDesc()
+    {
+        return _items.OrderByDescending(item => item).ToList();
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Generics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Group<int> group = new Group<int>();
+
+        group.AddAnItem(1);
+        group.AddAnItem(2);
+        group.AddAnItem(3);
+
+        var anItem = group.GetAnItem(1);
+
+        Console.WriteLine(anItem);
+
+        var sortedDesc = group.GetSortedListDesc();
+
+        foreach (var item in sortedDesc) 
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
